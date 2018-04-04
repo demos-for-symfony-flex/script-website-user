d9b091985e6a591735ea99377240750e4cd5bb42
Nardberjean <Nardberjean@users.noreply.github.com>
2018-04-04 08:47:43 -0400

    Update install.bash

--- install.bash
+++ install.bash
@@ -24,6 +24,6 @@ bin/console doctrine:migrations:diff --quiet
 bin/console doctrine:migrations:migrate --no-interaction --quiet
 # bin/console doctrine:schema:update --force
 composer require doctrine/doctrine-fixtures-bundle --dev
-bin/console doctrine:fixtures:load
+bin/console doctrine:fixtures:load --append
 
 bin/console assets:install --symlink
