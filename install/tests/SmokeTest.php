e4779396a5fc4a028dc81c7651d53eeda5b7a662
Nardberjean <Nardberjean@users.noreply.github.com>
2018-04-05 12:26:39 -0400

    Create SmokeTest.php

---
+++ install/tests/SmokeTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class SmokeTest extends WebTestCase
+{
+    public function testSomething()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertSame(200, $client->getResponse()->getStatusCode());
+        $this->assertSame(1, $crawler->filter('h1:contains("Hello World")')->count());
+    }
+
+    /**
+     * @dataProvider urlProvider
+     */
+    public function testPageIsSuccessful($url)
+    {
+        $client = self::createClient();
+        $client->request('GET', $url);
+
+        $this->assertTrue($client->getResponse()->isSuccessful());
+    }
+
+    public function urlProvider()
+    {
+        yield ['/login'];
+    }
+}
