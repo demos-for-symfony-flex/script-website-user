a6943208f3d5f3d28bd5491cae75390be4e80d98
Nardberjean <Nardberjean@users.noreply.github.com>
2018-04-04 20:35:49 -0400

    Update install.bash

--- install.bash
+++ install.bash
@@ -2,10 +2,11 @@
 # [[ ]] requires bash
 set -ev # https://docs.travis-ci.com/user/customizing-the-build/
 
+origin=$(pwd)
 composer create-project --no-install symfony/website-skeleton $CREATE_PROJECT_DIRECTORY
 cd $CREATE_PROJECT_DIRECTORY
 composer config bin-dir bin
-cp ../.env.dist . # Needs apparently to be done before install.
+cp $origin/.env.dist . # Needs apparently to be done before install.
 composer install
 install --directory config # Is it really needed?
 # composer require symfony/yaml # in symfony/website-skeleton
@@ -15,22 +16,22 @@ install --directory config # Is it really needed?
 # composer require symfony/orm-pack # in symfony/website-skeleton
 # composer require symfony/swiftmailer-bundle # in symfony/website-skeleton
 # composer require symfony/security-csrf
-cp ../config/packages/*.yaml config/packages --verbose
-cp ../config/routes/*.yaml config/routes --verbose
+cp $origin/config/packages/*.yaml config/packages --verbose
+cp $origin/config/routes/*.yaml config/routes --verbose
 composer require friendsofsymfony/user-bundle
 
-cp ../src/Entity/*.php src/Entity --verbose # May be done earlier.
+cp $origin/src/Entity/*.php src/Entity --verbose # May be done earlier.
 bin/console doctrine:database:create
 bin/console doctrine:migrations:diff --quiet
 bin/console doctrine:migrations:migrate --no-interaction --quiet
 # bin/console doctrine:schema:update --force
 # composer require doctrine/doctrine-fixtures-bundle --dev
-# cp ../src/DataFixtures/AppFixtures.php src/DataFixtures
+# cp $origin/src/DataFixtures/AppFixtures.php src/DataFixtures
 # bin/console doctrine:fixtures:load --append
 bin/console fos:user:create superadmin superadmin@example.com superadmin --super-admin
 bin/console fos:user:create user user@example.com user
 
-cp --recursive ../tests . --verbose
+cp --recursive $origin/tests . --verbose
 echo 'TODO: check this list...'
 # composer require --dev phpunit/phpunit
 # composer require --dev symfony/phpunit-bridge
